d1e5e0b6ca784241df7318fb88a5a32da2088c3f
yamoonz <yamooonz@gmail.com>
2020-12-15 04:20:38 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import React from "react";
-// components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
-// items
-
-// redux stuff
-
 import { createStore } from "redux";
-import reducer from "./reducer";
-// react-redux - Provider - wraps app , connect - used in components
+import reducer from "./reducer";  
 import { Provider } from "react-redux";
-// initial store
 
-// store
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 function App() {
-  // cart setup
-
   return (
+    <>
     <Provider store={store}>
       <Navbar />
       <CartContainer />
     </Provider>
+    
+    </>
   );
 }
 
