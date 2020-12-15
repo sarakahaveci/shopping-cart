e3337ec127ab7b5cb2e54c7963b2ed9a07b9afb7
yamoonz <yamooonz@gmail.com>
2020-12-15 04:21:56 +0300

    Updates

---
+++ precache-manifest.1a1b43b0378d0435021c53291bf719d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0786d917104e45c5ac8bae6c75defab",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "90b4c5f379c09fbdcaf1",
+    "url": "/shopping-cart/static/css/main.c0d7197d.chunk.css"
+  },
+  {
+    "revision": "2963abfcc3d5438e1eae",
+    "url": "/shopping-cart/static/js/2.b209bfb6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/shopping-cart/static/js/2.b209bfb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b4c5f379c09fbdcaf1",
+    "url": "/shopping-cart/static/js/main.d2cde4f8.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  }
+]);
